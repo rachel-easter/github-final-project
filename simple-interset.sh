83d89dae597a010f34034b713046359cdb199d98
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-04-29 19:32:02 +0530

    Update simple-interset.sh

--- simple-interset.sh
+++ simple-interset.sh
@@ -4,7 +4,8 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # rachel-easter
+   
    # Input:
    # p, principal amount
    # t, time period in years
